525ee13a0432ea5442f156209d100f59d80ab6e2
rodrigo-morais <morais.rm@gmail.com>
2018-06-24 15:00:45 +0200

    Adding tests and solution to two notes.

--- dojo.js
+++ dojo.js
@@ -1,8 +1,26 @@
 const NOTES = [5, 10, 20, 50]
 
 
-const withdraw = (value) => {
-  return { '5': 1 }
+const getMoney = (value, notes) => {
+  const smallest = Math.min(...notes.filter(note => note <= value))
+
+  if (value < smallest || notes.length === 0) {
+    return []
+  } else {
+    const newValue = value - smallest
+    const newNotes = newValue === smallest ? [smallest] : notes.slice(1)
+
+    return [{ [smallest] : 1 }].concat(getMoney(newValue, newNotes))
+  }
 }
+
+const withdraw = (value) =>
+  getMoney(value, NOTES).reduce((acc, note) => {
+    const existNote = Object.keys(acc).find(key => key === Object.keys(note)[0])
+
+      acc[Object.keys(note)[0]] =!!existNote ? acc[Object.keys(note)[0]] + 1 : 1
+
+    return acc
+  }, {})
 
 module.exports = { withdraw }
--- dojo.test.js
+++ dojo.test.js
@@ -7,4 +7,14 @@ describe('Household mix', () => {
       expect(withdraw(5)).toEqual({ '5': 1 })
     })
   })
+
+  describe('two notes', () => {
+    it('returns two of 5 when ask 10', () => {
+      expect(withdraw(10)).toEqual({ '5': 2 })
+    })
+
+    it('returns one of 5 and one of 10 when ask 15', () => {
+      expect(withdraw(15)).toEqual({ '5': 1, '10': 1 })
+    })
+  })
 })
